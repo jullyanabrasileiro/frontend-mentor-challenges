e0975dee69ae98e5550a924147abf1557489386b
jullyanabrasileiro <jullyana.brasileiro@hotmail.com>
2023-07-08 20:40:22 -0300

    wip

---
+++ 010 - Interactive rating component/scripts/scripts.js
@@ -0,0 +1,6 @@
+// Get the necessary elements
+const numberCircles = document.querySelectorAll('.number-circle');
+const submitButton = document.getElementById('submit');
+const thankYouDiv = document.getElementById('hidden-div');
+const selectedNumberSpan = document.querySelector('.selected-number');
+
