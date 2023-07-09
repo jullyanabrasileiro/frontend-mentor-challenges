c6f9f15cdfdd8d67a90dea3bf9e42cb1f0260fdc
jullyanabrasileiro <jullyana.brasileiro@hotmail.com>
2023-07-09 19:26:25 -0300

    Frontend Mentor Challenges

--- 010 - Interactive rating component/scripts/scripts.js
+++ 010 - Interactive rating component/scripts/scripts.js
@@ -1,6 +1,26 @@
-// Get the necessary elements
-const numberCircles = document.querySelectorAll('.number-circle');
-const submitButton = document.getElementById('submit');
-const thankYouDiv = document.getElementById('hidden-div');
-const selectedNumberSpan = document.querySelector('.selected-number');
+const numberCirclesDOM = document.querySelectorAll('.number-circle');
+const ratingCardDOM = document.querySelector('.rating');
+const thankyouCardDOM = document.querySelector('.thank-you');
+const selectedNumberDOM = document.querySelector('.selected-number');
 
+let activeRatingNumber;
+
+function selectRating(number) {
+    const selectedRatingDOM = numberCirclesDOM[number-1];
+
+    if(activeRatingNumber) {
+        const activeRatingDOM = numberCirclesDOM[activeRatingNumber-1];
+        activeRatingDOM.classList.remove('number-circle-active');
+    }
+
+    selectedRatingDOM.classList.add('number-circle-active');
+    activeRatingNumber = number;
+}
+
+function submit() {
+    if(!activeRatingNumber) return;
+    ratingCardDOM.classList.add('hidden');
+    thankyouCardDOM.classList.remove('hidden');
+
+    selectedNumberDOM.textContent = activeRatingNumber;
+}
